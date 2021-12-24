d93b1d2a566d0dde5b4c017bcf325a7114eac6cf
nazerise <94860981+nazerise@users.noreply.github.com>
2021-12-24 14:32:48 +0330

    Update mac-filtering.sh

--- mac-filtering.sh
+++ mac-filtering.sh
@@ -12,8 +12,11 @@ get_mac_address () {
 find_last_line () {
 	last_line=$(tail -n 1 /etc/dhcp/dhcpd.class)
 	check_line=$(tail -n 1 /etc/dhcp/dhcpd.class | grep hardware )
-        while [[ "$end" != "}" && -z $check_line ]]
+        while [[ "$end" != "}" ]]
         do
+		if [[ -z $check_line ]]; then
+			break
+		fi
                 echo -e "\033[0;31mNO last line \033[0m"
                 sed -i '$d' /etc/dhcp/dhcpd.class
                 end=$(tail -n 1 /etc/dhcp/dhcpd.class)
